43a17f302e92a828570aab228a20f35682cbc114
Mukarram Javid <mukarramjavid@gmail.com>
2021-01-19 20:33:03 +0500

    Add Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Home from "./Components/Home";
 import Sidebar from "./Components/Sidebar";
@@ -8,17 +8,24 @@ import About from "./Components/About";
 import Footer from "./Components/Footer";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+
+const Home = lazy(() => import('/Components/Home'));
+const About = lazy(() => import('/Components/About'));
+const Service = lazy(() => import('/Components/Service'));
+const Contact = lazy(() => import('/Components/Contact'));
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Sidebar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route exact path="/service" component={Service} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/Contact" component={Contact} />
-        </Switch>
+       <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route exact path="/service" component={Service} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/Contact" component={Contact} />
+          </Switch>
+        <Suspense/>
         <hr />
         <Footer />
       </BrowserRouter>
